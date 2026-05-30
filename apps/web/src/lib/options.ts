94c1a209f0d51d7ce21070293b59e0615b739e80
bhaskar-pi <bhaskar170863@gmail.com>
2026-05-30 20:29:16 +0530

    feat(cases): add case/court/hearing enum options

--- apps/web/src/lib/options.ts
+++ apps/web/src/lib/options.ts
@@ -1,4 +1,17 @@
-import { Designation, PracticeType } from "@splexa-group/shared/enums";
+import {
+  CaseStage,
+  CaseStatus,
+  CaseType,
+  ClientType,
+  CourtType,
+  Designation,
+  HearingPurpose,
+  HearingStatus,
+  ImportantDateType,
+  PartyRole,
+  PracticeType,
+  Priority,
+} from "@splexa-group/shared/enums";
 
 function toOptions<T extends Record<string, string>>(enumObj: T) {
   return Object.values(enumObj).map((value) => ({
@@ -10,5 +23,25 @@ function toOptions<T extends Record<string, string>>(enumObj: T) {
   }));
 }
 
+function toPascalOptions<T extends Record<string, string>>(enumObj: T) {
+  return Object.values(enumObj).map((value) => ({
+    value,
+    label: value
+      .replace(/([A-Z])/g, " $1")
+      .replace(/^_/, "")
+      .trim(),
+  }));
+}
+
 export const DESIGNATION_OPTIONS = toOptions(Designation);
 export const PRACTICE_TYPE_OPTIONS = toOptions(PracticeType);
+export const CASE_TYPE_OPTIONS = toPascalOptions(CaseType);
+export const CASE_STATUS_OPTIONS = toPascalOptions(CaseStatus);
+export const CASE_STAGE_OPTIONS = toPascalOptions(CaseStage);
+export const COURT_TYPE_OPTIONS = toPascalOptions(CourtType);
+export const PRIORITY_OPTIONS = toPascalOptions(Priority);
+export const PARTY_ROLE_OPTIONS = toPascalOptions(PartyRole);
+export const CLIENT_TYPE_OPTIONS = toPascalOptions(ClientType);
+export const HEARING_PURPOSE_OPTIONS = toPascalOptions(HearingPurpose);
+export const HEARING_STATUS_OPTIONS = toPascalOptions(HearingStatus);
+export const IMPORTANT_DATE_TYPE_OPTIONS = toPascalOptions(ImportantDateType);
